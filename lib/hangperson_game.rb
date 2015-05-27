b3e5d9dbe9116fbd1309c40183ec4d64087b90a6
sabotuer99 <sabotuer99@gmail.com>
2015-05-27 02:02:09 -0300

    initial commit

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -11,5 +11,37 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+  
+  def initialize(new_word)
+	@word = new_word
+	@guesses = ''
+	@wrong_guesses = ''
+  end
+  
+  def word
+	@word
+  end
+  
+  def guesses
+	@guesses
+  end
+  
+  def wrong_guesses
+	@wrong_guesses
+  end
+	
+  def guess(char)
+	if @guesses.include? char or @wrong_guesses.include? char
+		return false
+	end
+  
+	if @word.include? char
+		@guesses = @guesses + char
+		return true
+	else
+		@wrong_guesses = @wrong_guesses + char
+		return true
+	end
+  end
 
 end
